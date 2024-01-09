e9a8807292c419990b3302f82a10041514f39cb6
prasanthj2023 <prasanth.j@kalvium.community>
2024-01-09 12:09:31 +0530

    Updated

---
+++ quiz/src/Components/Quiz.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import '../CSS/Quiz.css'
+
+class Quiz extends React.Component{
+
+
+    render(){
+        return(
+            <div className="container">
+                <h1 className="heading-text">Question</h1>
+                <div className="number">
+                    <h4><span>4 </span>of<span> 5</span></h4>
+                </div>
+                <h2 className="question">The Question fff ff ff df ff</h2>
+                <div className="answers">
+                    <div className="options">A</div>
+                    <div className="options">B</div>
+                    <div className="options">C</div>
+                    <div className="options">D</div>
+                </div>
+                <div className="buttons">
+                    <div className="previous">
+                        <button>Previous</button>
+                    </div>
+                    <div className="next">
+                        <button>Next</button>
+                    </div>
+                    <div className="quit">
+                        <button>Quit</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Quiz;
